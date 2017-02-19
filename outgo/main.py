72f1cf70fbcf25f9a2d71df54fa36cd1c1333884
velvia1222 <velvia1222@gmail.com>
2017-02-20 00:59:04 +0900

    入力画面にy、nボタン等を追加

--- outgo/main.py
+++ outgo/main.py
@@ -4,19 +4,24 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 
-Window.size = (300, 400)
+Window.size = (300, 450)
 
 
 class PrototypeWidget(Widget):
+
+    def select_y(self):
+        if self.target_y.state == 'normal':
+            self.target_y.state = 'down'
+
+    def select_n(self):
+        if self.target_n.state == 'normal':
+            self.target_n.state = 'down'
 
     def input_number(self, numberButton):
         self.number_display.text += numberButton.text
 
     def backspace(self):
         self.number_display.text = self.number_display.text[:-1]
-
-    def clear(self):
-        self.number_display.text = ''
 
 
 class PrototypeApp(App):
