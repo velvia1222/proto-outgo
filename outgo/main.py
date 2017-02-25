46387bc1772560c7e8e90524948d1f48559980d0
velvia1222 <velvia1222@gmail.com>
2017-02-26 00:47:23 +0900

    カテゴリリストを追加

--- outgo/main.py
+++ outgo/main.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.core.text import LabelBase, DEFAULT_FONT
 from kivy.core.window import Window
+from kivy.resources import resource_add_path
+from kivy.uix.widget import Widget
 
+resource_add_path('/usr/share/fonts/truetype')
+LabelBase.register(DEFAULT_FONT, 'fonts-japanese-gothic.ttf')
 Window.size = (300, 450)
 
 
-class PrototypeWidget(Widget):
+class InputWidget(Widget):
+
+    def build_category(self):
+        category_list = self.get_category_list()
+        if len(category_list) > 0:
+            self.category_spinner.text = category_list[0]
+        self.category_spinner.values = self.get_category_list()
+
+    def get_category_list(self):
+        return ['外食費', '食費', '日用品', '娯楽', '光熱費', '家賃']
 
     def select_y(self):
         if self.target_y.state == 'normal':
@@ -17,17 +30,19 @@ class PrototypeWidget(Widget):
         if self.target_n.state == 'normal':
             self.target_n.state = 'down'
 
-    def input_number(self, numberButton):
-        self.number_display.text += numberButton.text
+    def input_number(self, number_btn):
+        self.number_display.text += number_btn.text
 
     def backspace(self):
         self.number_display.text = self.number_display.text[:-1]
 
 
-class PrototypeApp(App):
+class OutgoApp(App):
     def build(self):
-        return PrototypeWidget()
+        widget = InputWidget()
+        widget.build_category()
+        return widget
 
 
 if __name__ == '__main__':
-    PrototypeApp().run()
+    OutgoApp().run()
